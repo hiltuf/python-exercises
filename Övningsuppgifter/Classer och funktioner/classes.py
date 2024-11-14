830ca4469fa614fc6e2b226436fd87d9f41aa69c
hiltuf <hiltuf056@ihmeducation.se>
2024-11-14 13:28:12 +0100

    Classes update

--- Övningsuppgifter/Classer och funktioner/classes.py
+++ Övningsuppgifter/Classer och funktioner/classes.py
@@ -1,18 +1,63 @@
-class Car:
-    def __init__(self, brand):
-        self.brand = brand
+class Bankkonto:
+    def __init__(self, belopp):
+        self.belopp = belopp 
+        print(f"Start saldo: {self.belopp} kr")
 
-    def start(self):
-        return "The car is starting."
+    def insättning(self):
+        belopp = int(input("Ange insättningsbelopp: "))
+        self.belopp += belopp
+        print(f"Insättning på {belopp} kr genomfört. Nytt saldo: {self.belopp} kr") 
 
-class ElectricCar(Car):
-    def __init__(self, brand, battery_capacity):
-        super().__init__(brand)  # Anropar föräldraklassens konstruktor
-        self.battery_capacity = battery_capacity
-    def start(self):
-        return "The electric car is starting on electricity."
+    def uttag(self):
+        belopp = int(input("Ange uttagsbelopp: "))
+        if belopp <= self.belopp:
+            self.belopp -= belopp
+            print(f"Uttag på {belopp} kr genomfört. Nytt saldo: {self.belopp} kr")
+        else:
+            print("Uttaget misslyckades: otillräckligt saldo.")
+
+    def visa_saldo(self):
+        print(f"Visa saldo: {self.belopp} kr")
 
 
-electric_car = ElectricCar("Tesla", 75)
-print(electric_car.start())  # Output: "The electric car is starting on electricity."
+saldo = Bankkonto(100)
 
+saldo.insättning()
+saldo.uttag()
+saldo.visa_saldo()
+
+class Rektangel:
+    def __init__(self, längd, bredd):
+        self.längd = längd
+        self.bredd = bredd
+        print(f"Längd: {self.längd} Bredd: {self.bredd}")
+
+    def area(self):
+        arean = self.längd * self.bredd
+        print(f"Arean: {arean}")
+
+    def omkrets(self):
+        omkrets = self.längd + self.längd + self.bredd + self.bredd
+        print(f"Omkrets: {omkrets}")
+
+
+nummer = Rektangel(50, 50)
+nummer.area()
+nummer.omkrets()
+
+nummer2 = Rektangel(2, 4)
+nummer2.area()
+nummer2.omkrets()
+
+class Anställd:
+    def __init__(self, namn, timlön, arbetade_timmar):
+        self.namn = namn
+        self.timlön = timlön
+        self.arbetade_timmar = arbetade_timmar
+        print(f"{namn} tjänar i timmen: {timlön} kr och har arbetat {arbetade_timmar} timmar")
+    def beräkna_lön(self):
+        lön = self.timlön * self.arbetade_timmar
+        print("Lön:", lön, "kr")
+
+person = Anställd('Hilmer', 150, 10)
+person.beräkna_lön()
